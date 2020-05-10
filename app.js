7c78a0dafd3e80c9939ccc999f3750cc80504f16
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-05-10 13:56:17 -0500

    finish timing out section

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-function consoleMsg(string) {
-    console.log(string);
+// function consoleMsg(string) {
+//     console.log(string);
+// };
+
+// consoleMsg('hi');
+
+// setTimeout(function() {consoleMsg('hey');}, 2000);
+
+function getWords() {
+    console.log('hi');
+    setTimeout(function() {console.log('hey');}, 3000);
+    setTimeout(function() {console.log('harro');}, 5000);
+    setTimeout(function() {console.log('hiiii');}, 6000);
+
 };
 
-consoleMsg('hi');
-
-setTimeout(function() {consoleMsg('hey');}, 2000);
+getWords();
