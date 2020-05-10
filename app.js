67cb87e08cb3ed558c7556e743276367e46272d8
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-05-10 16:42:35 -0500

    finish callbacks and recursion section

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Timing Out Section
 // function consoleMsg(string) {
 //     console.log(string);
 // };
@@ -6,12 +7,30 @@
 
 // setTimeout(function() {consoleMsg('hey');}, 2000);
 
-function getWords() {
-    console.log('hi');
-    setTimeout(function() {console.log('hey');}, 3000);
-    setTimeout(function() {console.log('harro');}, 5000);
-    setTimeout(function() {console.log('hiiii');}, 6000);
+// function getWords() {
+//     console.log('hi');
+//     setTimeout(function() {console.log('hey');}, 3000);
+//     setTimeout(function() {console.log('harro');}, 5000);
+//     setTimeout(function() {console.log('hiiii');}, 6000);
+// };
+// getWords();
+
+// Callbacks and Recursion
+const done = () => {
+    console.log('Job\'s done!');
+};
+
+function countdown(num, callback) {
+    setTimeout(() => {
+        if (num > 0) {
+            console.log(num);
+            countdown(num - 1, callback);
+        } else {
+            callback();
+        }
+    }, 1000);
 
 };
 
-getWords();
+
+countdown(3, done);
